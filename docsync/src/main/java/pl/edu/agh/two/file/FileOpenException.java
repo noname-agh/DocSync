0865e4424913b5299c2ab21b126afd0a09eba9c5
Krzysiek Mitrega <mitrega@student.agh.edu.pl>
2011-10-27 11:47:13 +0200

    FileOpener interface and default implementation.

---
+++ docsync/src/main/java/pl/edu/agh/two/file/FileOpenException.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.two.file;
+
+public class FileOpenException extends Exception {
+
+	public FileOpenException() {
+		super();
+	}
+
+	public FileOpenException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FileOpenException(String message) {
+		super(message);
+	}
+
+	public FileOpenException(Throwable cause) {
+		super(cause);
+	}
+	
+}
