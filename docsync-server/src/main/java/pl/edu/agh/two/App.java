10b2684ba81bac11d752382655aa990dbc7c192f
Tomasz Zdybał <tmp@student.agh.edu.pl>
2011-11-03 11:25:22 +0100

    server maven project

---
+++ docsync-server/src/main/java/pl/edu/agh/two/App.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
