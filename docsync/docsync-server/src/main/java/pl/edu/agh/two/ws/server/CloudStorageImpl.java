61eec9c8204cdb4d6dc4f151036d889eacb63859
Marek Macura <mmacura@student.agh.edu.pl>
2011-11-10 11:44:21 +0100

    Task 51

--- docsync/docsync-server/src/main/java/pl/edu/agh/two/ws/server/CloudStorageImpl.java
+++ docsync/docsync-server/src/main/java/pl/edu/agh/two/ws/server/CloudStorageImpl.java
@@ -2,10 +2,17 @@ package pl.edu.agh.two.ws.server;
 
 import pl.edu.agh.two.ws.CloudFile;
 import pl.edu.agh.two.ws.CloudFileInfo;
+import pl.edu.agh.two.ws.CloudMetadata;
 import pl.edu.agh.two.ws.CloudStorage;
 
 import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebService(endpointInterface = "pl.edu.agh.two.ws.CloudStorage", serviceName = "CloudStorage")
 public class CloudStorageImpl implements CloudStorage {
@@ -37,7 +44,18 @@ public class CloudStorageImpl implements CloudStorage {
 
 	@Override
 	public void pushMetadata(CloudFileInfo fileInfo) {
-		throw new RuntimeException("Not implemented yet.");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("serverUnit");
+		EntityManager em = emf.createEntityManager();
+		
+				
+		CloudMetadata md = fileInfo.getMetadata();	
+	
+		
+		em.persist(md);
+		em.persist(fileInfo);
+		
+		em.close();
+		emf.close();
 	}
 
 }
