77b1b6df4f734dff852b2f88e3221a8738ec2140
Adam Rugala <adam@rugala.pl>
2011-11-10 16:16:17 +0100

    New IMetadata interface.

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/interfaces/IMetadata.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/interfaces/IMetadata.java
@@ -1,8 +1,11 @@
 package pl.edu.agh.two.interfaces;
 
 import java.io.Serializable;
+import java.util.Map;
 
 
 public interface IMetadata extends Serializable {
-
+	public Map<String, String> getMap();
+	public void addValue(String key, String value);
+	public String getValue(String key);
 }
