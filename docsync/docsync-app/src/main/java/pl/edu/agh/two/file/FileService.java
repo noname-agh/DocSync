763adfc5fc1390c98587b4d7ce32c2a9143710e4
mitrega <mitrega@student.agh.edu.pl>
2011-11-10 13:40:24 +0100

    End of task 48 implementation (FileService getAllFilesWithContent)

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/file/FileService.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/file/FileService.java
@@ -1,32 +1,38 @@
 package pl.edu.agh.two.file;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import pl.edu.agh.two.gui.pdf.PDFMetadata;
 import pl.edu.agh.two.interfaces.IFileService;
 import pl.edu.agh.two.ws.CloudFile;
 import pl.edu.agh.two.ws.CloudFileInfo;
 import pl.edu.agh.two.ws.CloudMetadata;
 import pl.edu.agh.two.ws.CloudStorage;
 
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-
 public class FileService implements IFileService {
 	public static final String wsUrl = "http://149.156.205.250:13733/CloudStorage?wsdl";
 	public static final String wsNamespace = "http://server.ws.two.agh.edu.pl/";
 	public static final String wsName = "CloudStorage";
 
+	private static final String storagePath = ".";
 	private static CloudStorage cloud;
 
 	public FileService() {
 		Service service = null;
 		try {
-			service = Service.create(new URL(wsUrl), new QName(wsNamespace, wsName));
+			service = Service.create(new URL(wsUrl), new QName(wsNamespace,
+					wsName));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
@@ -44,8 +50,13 @@ public class FileService implements IFileService {
 
 	@Override
 	public List<DocSyncFile> getAllFilesWithContent() {
-		// TODO: implementation
-		throw new RuntimeException("Not implemented yet!");
+		List<CloudFile> cloudFiles = cloud.getAllFilesWithContent();
+		List<DocSyncFile> list = new LinkedList<DocSyncFile>();
+		for (CloudFile cloudFile : cloudFiles) {
+			list.add(createDocSyncFileFromCloudFile(cloudFile));
+		}
+		return list;
+
 	}
 
 	@Override
@@ -85,18 +96,58 @@ public class FileService implements IFileService {
 		// Read in the bytes
 		int offset = 0;
 		int numRead = 0;
-		while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+		while (offset < bytes.length
+				&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
 			offset += numRead;
 		}
 
 		// Ensure all the bytes have been read in
 		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file " + file.getName());
+			throw new IOException("Could not completely read file "
+					+ file.getName());
 		}
 
 		// Close the input stream and return bytes
 		is.close();
 		return bytes;
+	}
+
+	private DocSyncFile createDocSyncFileFromCloudFile(CloudFile cloudFile) {
+
+		File file = new File(storagePath + System.getProperty("file.separator")
+				+ cloudFile.getName());
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			fileOutputStream.write(cloudFile.getContent());
+			fileOutputStream.close();
+
+			if (getExtension(cloudFile.getName()).equals("pdf")) {
+				PDFMetadata metadata = new PDFMetadata();
+				try {
+					metadata.setPageNo(Integer.parseInt(cloudFile.getMetadata()
+							.getMetadata().get("page")));
+				} catch (Exception e) {
+					metadata.setPageNo(1);
+				}
+
+				PDFDocSyncFile docSyncFile = new PDFDocSyncFile(
+						file.getAbsolutePath());
+				docSyncFile.setMeta(metadata);
+				return docSyncFile;
+			}
+			else {
+				throw new Exception("Unsported file extension");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	private String getExtension(String fileName) {
+		int i = fileName.lastIndexOf('.');
+		return i >= 0 ? fileName.substring(i + 1) : null;
 	}
 
 }
