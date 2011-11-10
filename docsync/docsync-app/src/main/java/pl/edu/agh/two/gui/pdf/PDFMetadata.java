8bc2c5756f742edd2bbc0db5766cc5d46afe6723
Adam Rugala <adam@rugala.pl>
2011-11-10 16:15:30 +0100

    Let's implement new IMetadata interface.

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/pdf/PDFMetadata.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/pdf/PDFMetadata.java
@@ -1,22 +1,23 @@
 package pl.edu.agh.two.gui.pdf;
 
+import pl.edu.agh.two.file.Metadata;
 import pl.edu.agh.two.interfaces.IMetadata;
 
-public class PDFMetadata implements IMetadata {
+public class PDFMetadata extends Metadata implements IMetadata {
 	private static final long serialVersionUID = -6005595238003881335L;
-	private int pageNo;
+	public static final String PAGE = "page";
 
 	public PDFMetadata() {
-		pageNo = 1;
+		super();
+		this.addValue(PAGE, "1");
 	}
 
 	public int getPageNo() {
-		return pageNo;
+		return Integer.parseInt(this.getValue(PAGE));
 	}
 
 	public void setPageNo(int pageNo) {
-		this.pageNo = pageNo;
+		this.addValue(PAGE, pageNo + "");
 	}
-
 
 }
