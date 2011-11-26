088f823f338f00828ac898cb5d0098f08457dfcb
Adam Rugala <adam@rugala.pl>
2011-11-26 19:04:06 +0100

    RSS Manager dialog works properly now.

---
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/actions/RSSManagerAction.java
@@ -0,0 +1,108 @@
+package pl.edu.agh.two.gui.actions;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.lang.model.util.Elements;
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import org.hibernate.mapping.Collection;
+
+import pl.edu.agh.two.gui.DocSyncGUI;
+
+public class RSSManagerAction implements ActionListener {
+	private static final String TITLE = "DocSync - RSS Manager";
+	
+	private JList list = new JList();
+	private DefaultListModel model = new DefaultListModel();
+
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+		final JDialog dialog = new JDialog(DocSyncGUI.getFrame(), TITLE, true);
+		dialog.setLayout(new BorderLayout());
+
+		list.setModel(model);
+		DefaultListSelectionModel selModel = new DefaultListSelectionModel();
+		selModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setSelectionModel(selModel);
+		dialog.add(new JScrollPane(list), BorderLayout.CENTER);
+
+		JPanel buttonsPane = new JPanel();
+		buttonsPane.setLayout(new FlowLayout());
+		dialog.add(buttonsPane, BorderLayout.SOUTH);
+
+		JButton buttonAdd = new JButton("Add");
+		buttonAdd.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String url = (String) JOptionPane
+						.showInputDialog(dialog, "Add new RSS", "Add new RSS",
+								JOptionPane.PLAIN_MESSAGE);
+				if (url.length() > 0) {
+					model.addElement(url);
+				}
+			}
+		});
+		buttonsPane.add(buttonAdd);
+
+		JButton buttonRemove = new JButton("Remove");
+		buttonRemove.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int selected = list.getSelectedIndex();
+				if (selected != -1) {
+					model.remove(selected);
+				}
+			}
+		});
+		buttonsPane.add(buttonRemove);
+
+		dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		dialog.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				saveData();
+				dialog.dispose();
+			}
+		});
+		
+		initData();
+
+		dialog.pack();
+		dialog.setVisible(true);
+
+	}
+
+	private void initData() {
+		// TODO - get data from webservice and add to model
+		model.addElement("rss 1");
+		model.addElement("rss 2");
+	}
+	
+	private void saveData() {
+		// TODO - save data from model (webservice)
+		for (Object rss: Collections.list(model.elements())) {
+			System.out.println("Saving " + rss.toString());
+		}
+		
+	}
+
+}
