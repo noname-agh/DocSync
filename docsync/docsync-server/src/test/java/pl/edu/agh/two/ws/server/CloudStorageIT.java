1a1d0c16be3fef83bb30742c37650548c9503e12
Tomasz Zdybał <tmp@student.agh.edu.pl>
2011-11-27 23:31:25 +0100

    Nieużywany port do testów integracyjnych

--- docsync/docsync-server/src/test/java/pl/edu/agh/two/ws/server/CloudStorageIT.java
+++ docsync/docsync-server/src/test/java/pl/edu/agh/two/ws/server/CloudStorageIT.java
@@ -1,14 +1,5 @@
 package pl.edu.agh.two.ws.server;
 
-import java.util.Map;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Endpoint;
-import javax.xml.ws.Service;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,54 +8,65 @@ import pl.edu.agh.two.ws.CloudFileInfo;
 import pl.edu.agh.two.ws.CloudMetadata;
 import pl.edu.agh.two.ws.CloudStorage;
 
-import static org.testng.AssertJUnit.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CloudStorageIT {
-	
-	private static final String SERVICE_URL = "http://localhost:8080/";
-	
+
+	private static final String SERVICE_URL = "http://localhost:8888/";
+
 	private Endpoint endpoint;
 	private CloudStorage proxy;
-	
+
 	@BeforeClass
 	public void setUp() throws MalformedURLException {
 		/* Publish service */
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testServerUnit");
 		endpoint = Endpoint.publish(SERVICE_URL, new CloudStorageImpl(emf));
-		
+
 		/* Create proxy to service */
-		Service service = Service.create(new URL(SERVICE_URL + "CloudStorage?wsdl") , new QName("http://server.ws.two.agh.edu.pl/", "CloudStorage"));
+		Service service = Service.create(new URL(SERVICE_URL + "CloudStorage?wsdl"), new QName(
+				"http://server.ws.two.agh.edu.pl/", "CloudStorage"));
 		proxy = service.getPort(CloudStorage.class);
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		if (endpoint != null) {
 			endpoint.stop();
 		}
 	}
-	
+
 	@Test
 	void test() {
 		Map<String, String> meta = new HashMap<String, String>();
 		meta.put("key", "value");
-				
+
 		CloudMetadata fileMeta = new CloudMetadata();
 		fileMeta.setMap(meta);
-		
+
 		CloudFile file = new CloudFile();
 		file.setName("name");
-		file.setContent(new byte[] {1,2,3});
+		file.setContent(new byte[]{1, 2, 3});
 		file.setMetadata(fileMeta);
-		
+
 		CloudFileInfo fileInfo = proxy.addFile(file);
-		
+
 		CloudFile retFile = proxy.getFileWithContent(fileInfo);
-		
+
 		assertEquals(file.getContent(), retFile.getContent());
 		assertEquals(file.getName(), retFile.getName());
 		assertEquals(0, retFile.getMetadata().getVersion());
 		assertEquals("value", retFile.getMetadata().getMap().get("key"));
 	}
-	
+
 }
