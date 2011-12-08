121e706033e29c46b5b1b474c48d7299483205e8
Jaroslaw Szczesniak <null@student.agh.edu.pl>
2011-12-08 21:43:54 +0100

    Task #91

---
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/actions/GetLogAction.java
@@ -0,0 +1,38 @@
+package pl.edu.agh.two.gui.actions;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import pl.edu.agh.two.gui.DocSyncGUI;
+
+public class GetLogAction implements ActionListener {
+	private static final String TITLE = "DocSync - Message Log";
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		final JDialog dialog = new JDialog(DocSyncGUI.getFrame(), TITLE, true);
+		dialog.setLayout(new BorderLayout());
+		
+		List<String> logList = DocSyncGUI.getLogList();
+		
+		JList list = new JList();
+		list.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 11));
+		list.setListData(logList.toArray());
+		
+	    JScrollPane scrollPane = new JScrollPane(list);
+
+	    dialog.add(scrollPane, BorderLayout.CENTER);
+
+	    dialog.setSize(640, 480);
+	    dialog.setVisible(true);
+	}
+
+}
