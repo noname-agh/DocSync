673395648684e50adb0a449f6fe6b5ab3edebf0e
Tomasz Zdybał <tmp@student.agh.edu.pl>
2011-12-08 12:09:13 +0100

    #87 - dodanie kolumny 'read'

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/RSSTableModel.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/gui/RSSTableModel.java
@@ -28,8 +28,9 @@ public class RSSTableModel extends AbstractTableModel implements IRSSList {
 	private static final String LINK_CAPTION = "Link";
 	private static final String DESCRIPTION_CAPTION = "Description";
 	private static final String CHANNEL = "Channel";
+	private static final String READ = "Read";
 	private static final String[] COLUMN_NAMES =
-			new String[]{TITLE_CAPTION, LINK_CAPTION, DESCRIPTION_CAPTION, CHANNEL};
+			new String[]{READ, TITLE_CAPTION, LINK_CAPTION, DESCRIPTION_CAPTION, CHANNEL};
 
 	private List<RSSItem> items = new ArrayList<RSSItem>();
 
@@ -52,6 +53,15 @@ public class RSSTableModel extends AbstractTableModel implements IRSSList {
 	}
 
 	@Override
+	public Class<?> getColumnClass(int i) {
+		if (i == 0) {
+			return Boolean.class;
+		} else {
+			return super.getColumnClass(i);
+		}
+	}
+
+	@Override
 	public int getColumnCount() {
 		// tytuł, link, opis (jeśli jest), kanał
 		return COLUMN_NAMES.length;
@@ -62,16 +72,18 @@ public class RSSTableModel extends AbstractTableModel implements IRSSList {
 		RSSItem item = getItem(i);
 
 		if (j == 0) {
-			return item.getTitle();
+			return item.getReaded();
 		} else if (j == 1) {
-			return item.getLink();
+			return item.getTitle();
 		} else if (j == 2) {
+			return item.getLink();
+		} else if (j == 3) {
 			if (item.getDescription() != null) {
 				return item.getDescription();
 			} else {
 				return "No description";
 			}
-		} else if (j == 3) {
+		} else if (j == 4) {
 			return item.getChannelAddress();
 		} else {
 			return null;
@@ -92,7 +104,7 @@ public class RSSTableModel extends AbstractTableModel implements IRSSList {
 
 		// get messages
 		RSSService service = RSSService.getInstance();
-		items.addAll( service.getAllRSSItems() );
+		items.addAll(service.getAllRSSItems());
 
 		// sort
 		sortItems(items);
