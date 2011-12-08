14fe106eb9de09d3a83bd5540ed99a88d0439b84
mitrega <mitrega@student.agh.edu.pl>
2011-12-08 11:39:58 +0100

    Define interface for getting CloudFileInfo and DocSyncFiles in IFileService

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/interfaces/IFileService.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/interfaces/IFileService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import pl.edu.agh.two.file.DocSyncFile;
+import pl.edu.agh.two.ws.CloudFileInfo;
 
 public interface IFileService {
 	public void sendFile(DocSyncFile file) throws IOException;
@@ -12,4 +13,8 @@ public interface IFileService {
 
 	public void pushMetadata(DocSyncFile file);
 	//public IMetadata pullMetadata(DocSyncFile file);
+	
+	public List<CloudFileInfo> getFilesWithoutContent();
+	
+	public List<DocSyncFile> getFiles(List<CloudFileInfo> cloudFileInfos);
 }
