edc6daa85107d6ad7a07098781614681d460ea0e
null <null@student.agh.edu.pl>
2012-01-05 19:57:14 +0100

    Fixed RSS handling.

--- docsync/docsync-server/src/main/java/pl/edu/agh/two/ws/server/RSSReader.java
+++ docsync/docsync-server/src/main/java/pl/edu/agh/two/ws/server/RSSReader.java
@@ -2,28 +2,29 @@ package pl.edu.agh.two.ws.server;
 
 import java.net.URL;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pl.edu.agh.two.ws.RSSItem;
-import de.nava.informa.core.ChannelIF;
-import de.nava.informa.core.ItemIF;
-import de.nava.informa.impl.basic.ChannelBuilder;
-import de.nava.informa.parsers.FeedParser;
-import java.util.LinkedList;
 import pl.edu.agh.two.ws.dao.RSSChannelDAO;
 import pl.edu.agh.two.ws.dao.RSSItemDAO;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+
 public class RSSReader {
-	private static final Logger LOGGER = LoggerFactory.getLogger(RSSReader.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(RSSReader.class);
 
 	private RSSReader() {
 		// example rss feeds at startup
-		//addChannel("http://xkcd.com/rss.xml");
-		//addChannel("http://planet.lisp.org/rss20.xml");
+		// addChannel("http://xkcd.com/rss.xml");
+		// addChannel("http://planet.lisp.org/rss20.xml");
 	}
 
 	private RSSChannelDAO rssChannelDAO;
@@ -33,28 +34,30 @@ public class RSSReader {
 		this.rssChannelDAO = rssChannelDAO;
 		this.rssItemDAO = rssItemDAO;
 	}
-	
+
 	public void updateAll() {
 		LOGGER.debug("Updating all rss channels.");
-		for(String channelAddress : getRssChannelList() ) {
+		for (String channelAddress : getRssChannelList()) {
 			update(channelAddress);
 		}
 	}
-	
-	public void update(String chanellAddress) {
-		try {		
-			URL url = new URL(chanellAddress);
-			ChannelIF channel = FeedParser.parse(new ChannelBuilder(), url);
-			Collection<ItemIF> items = channel.getItems();
-			//TODO: download only new items if possible
 
-			for(ItemIF item : items) {
+	public void update(String chanellAddress) {
+		try {
+			URL url = new URL(chanellAddress);
+
+			SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = input.build(new XmlReader(url));
+
+			Collection<SyndEntry> items = feed.getEntries();
+
+			for (SyndEntry item : items) {
 				RSSItem dbItem = new RSSItem();
-				dbItem.setGuid(item.getGuid().getLocation());  // .getLocation() returns <guid> tag value
+				dbItem.setGuid(item.getLink()); // .getLocation() returns <guid> tag value
 				dbItem.setTitle(item.getTitle());
 				dbItem.setChannelAddress(url.toString());
-				dbItem.setDate(item.getDate());
-				dbItem.setDescription(item.getDescription());
+				dbItem.setDate(item.getUpdatedDate()); // or .getPublishedDate() ?
+				dbItem.setDescription(item.getDescription().getValue());
 				dbItem.setLink(item.getLink().toString());
 
 				dbItem.setReaded(false);
@@ -62,7 +65,8 @@ public class RSSReader {
 				updateRSSItem(dbItem);
 			}
 		} catch (Exception e) {
-			LOGGER.error(String.format("Error when updating channel %s", chanellAddress), e);
+			LOGGER.error(String.format("Error when updating channel %s",
+					chanellAddress), e);
 		}
 		LOGGER.info("RSS Channel updated - " + chanellAddress);
 	}
@@ -71,12 +75,12 @@ public class RSSReader {
 		rssChannelDAO.addRSSChannel(new RSSChannel(address));
 		LOGGER.info("RSS Channel added - " + address);
 	}
-	
+
 	public void removeChannel(String address) {
 		rssChannelDAO.removeRSSChannel(new RSSChannel(address));
 		LOGGER.info("RSS Channel removed - " + address);
 	}
-	
+
 	public List<String> getRssChannelList() {
 		List<RSSChannel> channels = rssChannelDAO.getRSSChannels();
 		List<String> addresses = new LinkedList<String>();
@@ -86,13 +90,13 @@ public class RSSReader {
 		LOGGER.info("Query for RSS Channel list performed");
 		return addresses;
 	}
-	
+
 	public List<RSSItem> getRSSItems() {
 		List<RSSItem> rssItemList = rssItemDAO.getRSSItems();
 		LOGGER.debug("returning " + rssItemList.size() + " items");
 		return rssItemList;
 	}
-	
+
 	public void updateRSSItem(RSSItem item) {
 		RSSItem existingItem = rssItemDAO.findRSSItem(item.getGuid());
 		if (existingItem != null) {
