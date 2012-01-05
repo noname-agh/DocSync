19d187fe296c79a656dde481898697159879512a
Tomasz Zdybał <tmp@student.agh.edu.pl>
2012-01-05 22:44:14 +0100

    wczytywanie konfiguracji z pliku 'user.properties'

--- docsync/docsync-app/src/main/java/pl/edu/agh/two/utils/ConfigReader.java
+++ docsync/docsync-app/src/main/java/pl/edu/agh/two/utils/ConfigReader.java
@@ -3,6 +3,7 @@ package pl.edu.agh.two.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -16,6 +17,8 @@ import java.util.Properties;
 public class ConfigReader {
 	private static final Logger log = LoggerFactory.getLogger(ConfigReader.class);
 	private static ConfigReader ourInstance = new ConfigReader();
+	private static final String DEFAULT_CONFIG = "config.properties";
+	private static final String USER_CONFIG = "user.properties";
 
 	private Properties properties;
 
@@ -29,10 +32,23 @@ public class ConfigReader {
 
 	private ConfigReader() {
 		properties = new Properties();
+		// load default properties
 		try {
-			properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+			properties.load(getClass().getClassLoader().getResourceAsStream(DEFAULT_CONFIG));
 		} catch (IOException e) {
 			log.error("Error", e);
+		}
+
+		// try to load user - specific properties
+		Properties user = new Properties();
+		try {
+			user.load(new FileReader(USER_CONFIG));
+			for (String name : user.stringPropertyNames()) {
+				properties.put(name, user.get(name));
+			}
+			log.info("User defined properties loaded!");
+		} catch (IOException e) {
+			log.info("No user defined properties!");
 		}
 	}
 }
